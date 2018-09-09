7b0e5eeb98ab409f8d946b133a0ede6bc4208cab
Tejavardhan Reddy Meedimale <tmeedimale@gmail.com>
2018-09-09 23:51:54 +0530

    Service Discovery Changes

--- src/main/java/com/assignment/file/download/DownloadServiceApplication.java
+++ src/main/java/com/assignment/file/download/DownloadServiceApplication.java
@@ -4,9 +4,11 @@ import com.assignment.file.download.property.FileDownloadProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties({FileDownloadProperties.class})
+@EnableEurekaClient
 public class DownloadServiceApplication {
 
     public static void main(String[] args) {
