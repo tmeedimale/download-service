0fcb29d4aa3a4c86e442a83e0d1effc0883746ff
Tejavardhan Reddy Meedimale <tmeedimale@gmail.com>
2018-09-09 23:07:25 +0530

    Fix Build Issues

--- src/main/java/com/assignment/file/download/DownloadServiceApplication.java
+++ src/main/java/com/assignment/file/download/DownloadServiceApplication.java
@@ -1,9 +1,12 @@
 package com.assignment.file.download;
 
+import com.assignment.file.download.property.FileDownloadProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({FileDownloadProperties.class})
 public class DownloadServiceApplication {
 
     public static void main(String[] args) {
